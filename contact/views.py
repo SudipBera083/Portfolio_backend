13c2365e08423f9152942a4a2dcd91330974b625
sudip <sudipbera083@gmail.com>
2026-03-13 02:10:03 +0530

    Initial backend commit

---
+++ contact/views.py
@@ -0,0 +1,22 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import ContactMessage
+from .serializers import ContactMessageSerializer
+
+
+@api_view(['POST'])
+def send_message(request):
+
+    serializer = ContactMessageSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        return Response(
+            {"message": "Message sent successfully"},
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
